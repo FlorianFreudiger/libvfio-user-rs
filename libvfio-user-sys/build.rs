a1cb90a5ab55626021f2128e57bc5ef7fc53f09b
Florian Freudiger <25648113+FlorianFreudiger@users.noreply.github.com>
2024-01-29 20:24:58 +0100

    Update dependencies

--- libvfio-user-sys/build.rs
+++ libvfio-user-sys/build.rs
@@ -133,7 +133,7 @@ fn main() {
         .clang_arg("-fparse-all-comments")
         // Tell cargo to invalidate the built crate whenever any of the
         // included header files changed.
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         // Finish the builder and generate the bindings.
         .generate()
         // Unwrap the Result and panic on failure.
